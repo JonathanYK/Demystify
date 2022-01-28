6cf43f264ec5d0541e674dfca3fbbe962a26c89d
yk1993 <yk1993@gmail.com>
2022-01-28 19:14:50 +0200

    changed the logger behavior- instead of using the root logger- splitted to dedicated logger and 2 handlers: file_handler for printing to the .log file, and stream_handler for printing to the console

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
-from distutils.debug import DEBUG
+import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import false, func, true
+from sqlalchemy import func
 import logging
 
 app = Flask(__name__)
@@ -37,45 +37,81 @@ class Sessions(db.Model):
     def __repr__(self):
         return "#201: Session ID generated: " + str(self.id)
 
-
 def validate_json_keys(json_data):
 
     keys_dict = json_data.keys()
     must_have_keys = ["message", "additional", "level", "timestamp", "fileName", "lineNumber"]
     for key in must_have_keys:
         if key not in keys_dict:
-            return false
+            return False
 
-    return true
+    return True
     
-    
+
+
+def print_CRITICAL(logger, logging_data):
+    for data in logging_data:
+        logger.critical(data)
+
+def print_ERROR(logger, logging_data):
+    for data in logging_data:
+        logger.error(data)
+
+def print_WARNING(logger, logging_data):
+    for data in logging_data:
+        logger.warning(data)
+
+def print_INFO(logger, logging_data):
+    for data in logging_data:
+        logger.info(data)
+
+def print_DEBUG(logger, logging_data):
+    for data in logging_data:
+        logger.debug(data)
+
+def print_NOTSET(logger, logging_data):
+    for data in logging_data:
+        logger.notset(data)
 
 
 # logging the data to a .log file:
-def log_data(session_id, json_data):
+def log_data(logger, session_id, json_data):
+
+    logging_data = []
 
     # parsering the json params:
+
     str_log_level = json_data["level"]
-    time_stamp = json_data["timestamp"]
-    file_name = json_data["fileName"]
-    line_number = json_data["lineNumber"]
+    logging_data.append(str_log_level)
+    logging_data.append(json_data["timestamp"])
+    logging_data.append(json_data["fileName"])
+    logging_data.append(json_data["lineNumber"])
 
-    # logging configuration and printings:
-    logging.basicConfig(filename=session_id+".log", filemode="a", level=logging.DEBUG)
+    dynamic_log_printer = globals()["print_%s" % str_log_level]
+    logger.setLevel(logging.getLevelName(str_log_level))
+    session_formatter = logging.Formatter("%(asctime)s::%(levelname)s::%(name)s::%(message)s")
+    
+    file_handler = logging.FileHandler(filename=session_id+".log", mode="a")
+    file_handler.set_name(session_id)
+    file_handler.setFormatter(session_formatter)
 
-    logging.debug(str_log_level)
-    logging.debug(time_stamp)
-    logging.debug(file_name)
-    logging.debug(line_number)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(session_formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+    dynamic_log_printer(logger, logging_data)
+
+    logger.removeHandler(file_handler)
+    logger.removeHandler(stream_handler)
 
 
 @app.route("/")
 def home():
     return "Main demystify page"
 
-
 @app.route("/session_id", methods=["GET"])
-
 def session_id_generator():
     
     # configure db and get a session id:
@@ -93,26 +129,25 @@ def session_id_generator():
             return "#400: There was an issue adding new session id to the db!"
 
 
-# TODO: add validations to provided JSON params! 
-# TODO: return the relevant http code! 
 @app.route("/log", methods=["POST"])
 def log():
+    logger = logging.getLogger("__name__")
     # retriving the session id from the url:
     curr_session_id = request.args.get("session_id")
 
-    if session_id_validation(curr_session_id) is None:
+    # validate the session id:
+    if session_id_validation(curr_session_id) is False:
         return "#400: The provided session id isn't valid!"
     
-    if validate_json_keys(request.json) is false:
+    # validate there arn't missing required keys in json file:
+    if validate_json_keys(request.json) is False:
         return "#400: There is a missing key/s in the provided json!"
 
-    log_data(curr_session_id, request.json)
+    # log the date to a dedicated file (session_id.log):
+    log_data(logger, curr_session_id, request.json)
 
     return "#200: logging done!"
-
-
 
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
-    
