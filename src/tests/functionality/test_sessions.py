7125a25d413f327c1fec6b299ef59d001241d005
yk1993 <yk1993@gmail.com>
2022-04-09 14:54:13 +0300

    minor comment change for testing github actions with changes

--- src/tests/functionality/test_sessions.py
+++ src/tests/functionality/test_sessions.py
@@ -2,7 +2,7 @@ def test_main_page(test_client):
     """
     GIVEN a Flask application configured for testing
     WHEN the '/' page is requested (GET)
-    THEN check the response is valid
+    THEN check the response is valid.
     """
 
     response = test_client.get('/')
@@ -23,7 +23,7 @@ def test_sessions_id_generator_page(test_client, return_session_id=False):
 
     assert ret_lst[0] == "Session ID generated"
     
-    # gen_session_id has to be exactly 9 digits:
+    #gen_session_id has to be exactly 9 digits:
     assert len(str(gen_session_id)) == 9
     assert response.status_code == 201
 
