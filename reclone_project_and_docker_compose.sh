fa7761f3af7f38feb8ce283d4fc358207ec3b604
JonathanYK <48648513+JonathanYK@users.noreply.github.com>
2022-04-10 21:35:47 +0300

    Create reclone_project_and_docker_compose.sh

---
+++ reclone_project_and_docker_compose.sh
@@ -0,0 +1,21 @@
+# This shell file should be placed in /home/ubuntu/src in order to restart the docker compose (reclone the project and pull docker image).
+
+date -u
+
+echo "entring main path"
+cd /home/ubuntu
+
+echo "compose down"
+sudo docker-compose -f ~/Sessions_Project/src/docker-compose.yml down
+
+echo "removing old clone"
+rm -rf Sessions_Project/
+
+echo "cloning ssh from JonathanYK/Sessions_Project.git"
+git clone git@github.com:JonathanYK/Sessions_Project.git
+
+echo "entering cloned project"
+cd ./Sessions_Project/src
+
+echo "compose up"
+sudo docker-compose up -d
