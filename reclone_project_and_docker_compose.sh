ed43ca8404162cfbf7f04db05ffeb824fe27437b
JonathanYK <48648513+JonathanYK@users.noreply.github.com>
2022-04-11 19:19:17 +0300

    Update reclone_project_and_docker_compose.sh
    added elaborated explanation on crontab, updated shell location to be automatically copied to dedicate destanation

--- reclone_project_and_docker_compose.sh
+++ reclone_project_and_docker_compose.sh
@@ -1,4 +1,13 @@
-# This shell file should be placed in /home/ubuntu/src in order to restart the docker compose (reclone the project and pull docker image).
+# This shell file should be configured on crontab in order add scheduled task to restart the docker compose (reclone the project and pull docker image):
+
+# add a scheduled task using crontab:
+# $crontab -e
+# Add  the following command (to be executed on each day at 00:15):
+# 15 0 * * * /home/ubuntu/reclone_shell/reclone_project_and_docker_compose.sh >> /home/ubuntu/cron.log 2>&1
+
+# or this one for each 5 minutes (for debug):
+# 0,5,10,15,20,25,30,35,40,45,50,55 * * * * /home/ubuntu/reclone_shell/reclone_project_and_docker_compose.sh >> /home/ubuntu/cron.log 2>&1
+
 
 echo "------------------"
 date -u
@@ -16,18 +25,14 @@ rm -rf Sessions_Project/
 echo "cloning ssh from JonathanYK/Sessions_Project.git"
 git clone git@github.com:JonathanYK/Sessions_Project.git
 
+echo "create dedicated dir for reclone_project_and_docker_compose.sh"
+mkdir -p /home/ubuntu/reclone_shell
+
+echo "copy reclone_project_and_docker_compose.sh to /home/ubuntu/reclone_shell"
+cp /home/ubuntu/Sessions_Project/reclone_project_and_docker_compose.sh /home/ubuntu/reclone_shell
+ 
 echo "entering cloned project"
 cd ./Sessions_Project/src
 
 echo "compose up"
 sudo docker-compose up -d
-
-
-
-# lastly, add a schedule using crontab:
-# $crontab -e
-# Add there the following command (for each day at 00:15):
-# 15 0 * * * /home/ubuntu/src/reclone_project_and_docker_compose.sh >> /home/ubuntu/cron.log 2>&1
-
-# or this one for each 5 minutes (for debug):
-# 0,5,10,15,20,25,30,35,40,45,50,55 * * * * /home/ubuntu/src/reclone_project_and_docker_compose.sh >> /home/ubuntu/cron.log 2>&1
