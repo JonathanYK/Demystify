69a6770de704bd603bb437104595f4ef877ac147
JonathanYK <48648513+JonathanYK@users.noreply.github.com>
2022-04-13 19:22:09 +0300

    Update conftest.py

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -3,7 +3,7 @@ from project import create_app
 
 @pytest.fixture(scope='module')
 def test_client():
-    flask_app = create_app('instance/flask_test.cfg')
+    flask_app = create_app('src/instance/flask_test.cfg')
 
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as testing_client:
